13559d23bc5bf5c36ffe2ff10805b4b595ea0f53
Shahnawaz Ali <saalis.uk@gmail.com>
2023-01-31 00:08:27 +0000

    initial commit

---
+++ static/index.js
@@ -0,0 +1,1 @@
+console.log('This is java script running..!')
---
+++ views.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+
+views = Blueprint(__name__, "views")
+
+
+# add home route
+@views.route("/")
+def home():
+    return render_template("index.html", name="Ali", age=36, occupation='Researcher')
+
+# add profile route
+# use query parameter
+@views.route("/profile/")
+def profile():
+    args = request.args
+    name = args.get("name")
+    return render_template("profile.html", name=name)
+
+# add about route
+@views.route("/about")
+def about():
+    return render_template("about.html")
+
+# add json route an return in json format
+@views.route("/json_example")
+def get_json():
+    atrribute_dict = {
+        "user_id":"11001",
+        "user_name":"Ali", 
+        "user_age": "36",
+        "user_ocuupation": "Researcher"
+    }
+    return jsonify(atrribute_dict)
+
+#add another endpoint data to get the data and return jsonified
+@views.route("/data")
+def get_data():
+    data = request.json
+    return jsonify(data)
+
+# redirect
+@views.route("/goToHome")
+def goToHome():
+    return redirect(url_for("views.home"))
