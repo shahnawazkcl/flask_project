2c977a3098b987447f561c67bae62d956cedc3f5
Shahnawaz Ali <saalis.uk@gmail.com>
2023-02-01 18:14:15 +0000

    Add inititial config application factory

--- flaskr/app.py
+++ flaskr/app.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from views import views
+import __init__
 
-app = Flask(__name__)
+
+app = __init__.create_app()
 app.register_blueprint(views, url_prefix="/")
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
