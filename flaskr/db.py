3167b26b623c89769b8871d798327e1aefab9c8b
Shahnawaz Ali <saalis.uk@gmail.com>
2023-02-08 20:29:47 +0000

    add temp layout and db function

---
+++ flaskr/db.py
@@ -0,0 +1,22 @@
+import sqlite3
+import click
+
+from flask import current_app, g
+
+# store and resue the connection to the database
+def get_db():
+    if 'db' not in g:
+        g.db = sqlite3.connect(
+            current_app.config['DATABASE'],
+            detect_types=sqlite3.PARSE_DECLTYPES
+        )
+        g.db.row_factory=sqlite3.Row
+
+    return g.db
+
+# check the if the db open then close before request
+def close_db(e=None):
+    db = g.pop('db', None)
+    if db is not None:
+        db.close()
+
